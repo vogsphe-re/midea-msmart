090c0ab0bfb372876d735658e2802f06b4708d01
Tucker Kern <tuckkern@gmail.com>
2023-09-14 04:57:13 +0000

    Add decrypted Lua for protocol

---
+++ reference/decode_lua.py
@@ -0,0 +1,28 @@
+from hashlib import sha256
+from Crypto.Cipher import AES
+import sys
+
+APP_KEY = "ac21b9f9cbfe4ca5a88562ef25e2b768"
+
+def encode_app_key() -> bytes:
+  hash = sha256(APP_KEY.encode()).hexdigest()
+  return hash[:16].encode()
+
+def encode_iv() -> bytes:
+  hash = sha256(APP_KEY.encode()).hexdigest()
+  return hash[16:32].encode()
+
+def decrypt_lua(data) -> str:
+  key = encode_app_key()
+  iv = encode_iv()
+  return AES.new(key, AES.MODE_CBC, iv=iv).decrypt(data).decode("utf-8")
+
+with open(sys.argv[1]) as f:
+  data = bytes.fromhex(f.readlines()[0])
+  decrypted = decrypt_lua(data)
+
+with open(sys.argv[1] + ".dec", "w") as f:
+   f.write(decrypted)
+
+
+
